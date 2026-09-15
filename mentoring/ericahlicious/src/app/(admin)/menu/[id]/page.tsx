92ec1ee8f0519f8ccf6282b070d9d01bea1e4d9f
tulabotjianbsis-hub <tulabot.jian.bsis@gmail.com>
2026-09-15 22:02:42 +0800

    update

--- mentoring/ericahlicious/src/app/(admin)/menu/[id]/page.tsx
+++ mentoring/ericahlicious/src/app/(admin)/menu/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams, useRouter } from "next/navigation";
 import {
   getMenuItemRecipe,
